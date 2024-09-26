940ac4fb580e282eba968a78f468ffad35cffa29
zRosies <gustavo.enrollment@gmail.com>
2024-09-25 21:45:15 -0300

    saving project

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
+using BlazingPizza.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddDbContext<PizzaStoreContext>(options =>
+    options.UseSqlite("Data Source=pizza.db"));
+builder.Services.AddControllers();  // Add this if you are using controllers
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+}
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
 }
 
 app.UseStaticFiles();
@@ -16,5 +36,6 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
